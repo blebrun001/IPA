e66fc502cd350237f824f86662c22ac09a6802b0
blebrun001 <blebrun@iphes.cat>
2025-02-06 17:17:50 +0100

    Initial Commit

---
+++ Pizza/PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApp.swift
+//  Pizza
+//
+//  Created by Brice Lebrun on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PizzaTests/PizzaTests.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaTests.swift
+//  PizzaTests
+//
+//  Created by Brice Lebrun on 06/02/2025.
+//
+
+import Testing
+@testable import Pizza
+
+struct PizzaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
